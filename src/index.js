e6006213891f898900dd91a2ec884b767e5d87b9
alan1001110 <alan1001110@gmail.com>
2020-11-07 16:08:42 -0800

    New endless cycle direction changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import "./move-rt.css";
 import "./move-lf.css";
 document.getElementById("stripes").className = "rt";
-document.getElementById("stripes").className = "lf";
+
+var directions = ["rt", "lf"];
+var myCycle = [];
+
+function cycleDirections() {
+  if (myCycle.length === 0) {
+    myCycle = directions.slice();
+  }
+  document.getElementById("stripes").className = myCycle.shift();
+}
+
+window.setInterval(cycleDirections, 30000);
